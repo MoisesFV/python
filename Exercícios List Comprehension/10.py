138e559f04da87897ae4150fa914aba329328d23
MoisesFV <fagundes.moises@gmail.com>
2023-11-19 17:12:27 -0300

    Mais um exercício adicionado

---
+++ Exercícios List Comprehension/10.py
@@ -0,0 +1,44 @@
+# copy, sorted, produtos.sort
+# Exercícios
+# Aumente os preços dos produtos a seguir em 10%
+# Gere novos_produtos por deep copy (cópia profunda)
+
+import copy
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+# Ordene os produtos por nome decrescente (do maior para menor)
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+# Ordene os produtos por preco crescente (do menor para maior)
+# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+
+
+novos_produtos = copy.deepcopy(produtos)
+
+novos_produtos = [{**produto, 'preco':round(produto['preco']*1.1,2)}
+for produto in produtos
+]
+
+novos_produtos2 = copy.deepcopy(produtos)
+
+novos_produtos2 = [{**produto, 'preco':round(produto['preco']*1.1,2)}
+for produto in produtos
+]
+
+novos_produtos = sorted(novos_produtos, key=lambda d: d['nome'])
+print(novos_produtos)
+
+novos_produtos2 = sorted(novos_produtos2, reverse=True, key=lambda d: d['preco'])
+print(novos_produtos2)
+
+
+print(produtos)
+    
